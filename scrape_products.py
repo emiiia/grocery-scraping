935ce28d17c620e15ad897cbf00c3273846797fc
Emilia Lukose <emilia24k@gmail.com>
2023-08-27 12:48:45 +0100

    Get product titles from list items

--- scrape_products.py
+++ scrape_products.py
@@ -11,28 +11,46 @@ class ShopScraper(object):
         self.driver = webdriver.Firefox(options=options)
 
     def scrape(self, url, shop_id):
-        print("Getting", url)
+        product_list = []
+        print("Scraping", url)
         self.driver.get(url)
         html = self.driver.page_source
         soup = BeautifulSoup(html, "html.parser")
 
-        print(soup)
+        # Get product list items
+        li_tags = soup.find_all(
+            lambda tag: tag.name == "li"
+            and "product-list--list-item" in " ".join(tag.get("class", []))
+        )
 
-        product_list = [
-            {
-                "name": "Kellogg's Crunchy Nut Breakfast Cereal 500g",
-                "price": 4,
-                "discounted_price": 3,
-                "rating": 2,
-                "featured": 0,
-                "vegetarian": 0,
-                "vegan": 0,
-                "in_stock": 0,
-                "brand": "Crunchy Nut",
-                "company": "Kellog's",
-                "shop_id": shop_id,
-            }
-        ]
+        for li in li_tags:
+            # Get product titles
+            title_link = li.find(
+                lambda tag: tag.name == "a"
+                and any(
+                    "product-tile--title" in str(value)
+                    for _, value in tag.attrs.items()
+                )
+            )
+
+            if not title_link or not title_link.text or not title_link.text.strip():
+                continue
+
+            product_list.append(
+                {
+                    "name": title_link.text.strip(),
+                    "price": 4,
+                    "discounted_price": 3,
+                    "rating": 2,
+                    "featured": 0,
+                    "vegetarian": 0,
+                    "vegan": 0,
+                    "in_stock": 0,
+                    "brand": "Crunchy Nut",
+                    "company": "Kellog's",
+                    "shop_id": shop_id,
+                }
+            )
 
         return product_list
 
