82f440c72fb4d5aeba69b42bb70b6afd79442bb8
emiiia <emilia24k@gmail.com>
2023-08-28 15:46:36 +0100

    Log scraping time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from manage_db import GroceriesDB
 from scrape_products import ShopScraper
 import sys, os
+import time
 
 
 def main():
@@ -15,8 +16,13 @@ def main():
 
         # Get Tesco products
         shop_ids = db.get_shop_ids()
+
+        t1_start = time.perf_counter()
         product_list = scraper.scrape_tesco_brands(shop_ids.get("tesco"))
         product_list += scraper.scrape_ocado_brands(shop_ids.get("ocado"))
+        t1_stop = time.perf_counter()
+        time_taken_mins = (t1_stop - t1_start) / 60
+        print("Elapsed scraping time mins:", time_taken_mins)
 
         # Insert to Product table
         db.insert_products(product_list)
